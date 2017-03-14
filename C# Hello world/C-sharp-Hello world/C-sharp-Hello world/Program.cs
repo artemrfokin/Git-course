557e09045788fa9e3bc58eb47ecd5ab40be87430
artemrfokin <artemrfokin@gmail.com>
2017-03-14 22:06:40 +0300

    Added what's up.

---
+++ C# Hello world/C-sharp-Hello world/C-sharp-Hello world/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("what's up?");
+            Console.ReadLine();
+        }
+    }
+}
